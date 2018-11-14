6891faa745f25a0bffdd37321cb3c5466a0161c8
Alex <32816774+alex85120@users.noreply.github.com>
2018-11-14 18:37:11 -0500

    Add info on how rbs.txt is formatted

--- Rainbows/rainbows.py
+++ Rainbows/rainbows.py
@@ -6,6 +6,15 @@ from pygame.locals import QUIT
 from collections import Counter
 
 # Get all rainbows from rbs.txt, which (for now) is updated manually.
+# An entry in rbs.txt is created in the following format:
+# RED HEX CODE
+# ORANGE HEX CODE
+# YELLOW HEX CODE
+# GREEN HEX CODE
+# BLUE HEX CODE
+# PURPLE HEX CODE
+# USER WHO FINISHED THE RAINBOW
+# If any of the fields are yet to be filled, add "--TBD--" without the quotes.
 with open('rbs.txt') as f:
     rainbows = f.read().splitlines()
 
